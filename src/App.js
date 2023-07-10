5b59002293a625c8064081c822c80287119313e1
Catalin Tugui <katamaxx23@gmail.com>
2023-07-10 21:16:37 +0300

    main static components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+function Logo() {
+  return <h1>Travel pack 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>something will be here</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">list</div>;
+}
+
+function Stats() {
+  return <footer>You have X items on your list and you packed X%</footer>;
+}
+
 function App() {
-  return <div className="App">HI!</div>;
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
 }
 
 export default App;
