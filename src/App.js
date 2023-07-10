d76ae4345ad538a3d145ed6608f7553d59c2040d
Catalin Tugui <katamaxx23@gmail.com>
2023-07-10 21:31:32 +0300

    created static list and added styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function Logo() {
   return <h1>Travel pack 🧳</h1>;
 }
@@ -11,17 +16,43 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  );
 }
 
 function Stats() {
-  return <footer>You have X items on your list and you packed X%</footer>;
+  return (
+    <footer className="stats">
+      You have X items on your list and you packed X%
+    </footer>
+  );
 }
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
