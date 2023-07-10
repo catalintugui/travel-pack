6281359a0259302d9d9fd1be72a271cc399e92b3
Catalin Tugui <katamaxx23@gmail.com>
2023-07-11 00:25:59 +0300

    added form inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 3, packed: false },
 ];
 
 function Logo() {
@@ -8,10 +8,23 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>something will be here</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Add item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -20,7 +33,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
